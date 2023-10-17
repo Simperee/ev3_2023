793af9bd690593567bad8c17185c6aba1903c8ae
KotletToNieOwoc <118076351+KotletToNieOwoc@users.noreply.github.com>
2023-10-17 13:27:26 +0200

    I love dicks

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, GyroSensor, ColorSensor
+from pybricks.parameters import Port
+from pybricks.robotics import DriveBase
+
+# Initialize the EV3 Brick.
+ev3 = EV3Brick()
+
+# Initialize the motors.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+gyro = GyroSensor(Port.S1)
+color_sensor = ColorSensor(Port.S2)
+
+# Initialize the drive base.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=68.8, axle_track=127)
+
+gyro.reset_angle(0)
+
 kp = 1.0 #proportional constant
 speed = 50
 
@@ -23,6 +42,7 @@ def calibrateColorSensor():
 
     return (max + min) / 2
 
+
 def followLine():
 
     sensorValue = color_sensor.reflection()
@@ -31,13 +51,13 @@ def followLine():
 
     steeringValue = error * kp
 
-    motor_left.run(speed + steeringValue)
-    motor_right.run(speed - steeringValue)
+    left_motor.run(speed + steeringValue)
+    right_motor.run(speed - steeringValue)
 
     #wait(1)
 
+
 targetValue = calibrateColorSensor()
 
-while(true){
+while True:
     followLine()
-}
